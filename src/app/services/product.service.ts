4f06a965e05fc2189b97c1406ad1882470d34490
Viktor Lachkov <EPetrov2@gmail.com>
2022-12-02 16:32:34 +0400

    Add global error message handler

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,24 +1,36 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http'
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from '@angular/common/http';
 import { IProduct } from '../models/product';
-import { catchError, delay, Observable, throwError } from 'rxjs';
-
+import { catchError, delay, Observable, retry, throwError } from 'rxjs';
+import { ErrorService } from './error.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
-  constructor(private http: HttpClient) { }
-
-  private errorHandler(error: HttpErrorResponse) {
-    return throwError(() => error.message);
-  }
+  constructor(private http: HttpClient, private errorService: ErrorService) {}
 
   getAll(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>('https://fakestoreapi.com/products',
-    { params: new HttpParams({
-      fromObject: { limit: 7}
-    })}).pipe(catchError(this.errorHandler));
+    return this.http
+      .get<IProduct[]>('https://fakestoreapi.com/products', {
+        params: new HttpParams({
+          fromObject: { limit: 7 },
+        }),
+      })
+      .pipe(
+        retry(2),
+        catchError(this.errorHandler.bind(this))
+      );
+  }
+
+  private errorHandler(error: HttpErrorResponse) {
+    console.log(error.message);
+
+    this.errorService.handle(error.message);
+    return throwError(() => error.message);
   }
 }
