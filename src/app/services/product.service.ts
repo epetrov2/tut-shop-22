0eca474518435bc05ad24dbd949bf67eb3308d63
Viktor Lachkov <EPetrov2@gmail.com>
2022-12-02 15:56:21 +0400

    Add errorHandler for http calls

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http'
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http'
 import { IProduct } from '../models/product';
-import { delay, Observable } from 'rxjs';
+import { catchError, delay, Observable, throwError } from 'rxjs';
 
 
 @Injectable({
@@ -11,10 +11,14 @@ export class ProductService {
 
   constructor(private http: HttpClient) { }
 
+  private errorHandler(error: HttpErrorResponse) {
+    return throwError(() => error.message);
+  }
+
   getAll(): Observable<IProduct[]> {
     return this.http.get<IProduct[]>('https://fakestoreapi.com/products',
     { params: new HttpParams({
       fromObject: { limit: 7}
-    })});
+    })}).pipe(catchError(this.errorHandler));
   }
 }
