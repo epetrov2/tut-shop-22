d4f8a879622f4047dd20a8ae26d153e7417097d1
Viktor Lachkov <EPetrov2@gmail.com>
2022-12-04 18:15:20 +0400

    Add modal service shown on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { delay, Observable, tap } from 'rxjs';
 import { IProduct } from './models/product';
+import { ModalService } from './services/modal.service';
 import { ProductService } from './services/product.service';
 
 
@@ -14,7 +15,10 @@ export class AppComponent implements OnInit{
 
   term = '';
 
-  constructor(private productService: ProductService) { }
+  constructor(
+    private productService: ProductService,
+    public modalService: ModalService
+    ) { }
 
   products$?: Observable<IProduct[]>;
 
